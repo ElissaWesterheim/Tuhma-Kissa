5d0dae65ea9fc2e4e9378f4d80342d657b50d599
ElissaWesterheim <eew2000@hw.ac.uk>
2024-05-30 13:01:19 +0100

    talo audio

---
+++ Assets/taloAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class taloAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+
+    private void OnMouseDown()
+    {
+       
+            AudioSource.PlayClipAtPoint(_clip, transform.position);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
