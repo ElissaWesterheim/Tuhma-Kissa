598d381a730e146a3947635be8ecb02152728a91
ElissaWesterheim <eew2000@hw.ac.uk>
2024-06-08 18:59:00 +0100

    lots of things

---
+++ Assets/AudioSettings.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine;
+
+//code from https://www.youtube.com/watch?v=G-JUp8AMEx0
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider voiceSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume") && PlayerPrefs.HasKey("voiceVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+    }
+    public void SetMusicVolume()
+    {
+        float musicVolume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(musicVolume)* 20);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
+
+        float voiceVolume = voiceSlider.value;
+        myMixer.SetFloat("voice", Mathf.Log10(voiceVolume)* 20);
+        PlayerPrefs.SetFloat("voiceSlider", voiceVolume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");   
+        voiceSlider.value = PlayerPrefs.GetFloat("voiceVolume");
+        SetMusicVolume(); 
+    }
+
+
+}
---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int treat = 0;
+    private int verb = 0;
+    
+    //[SerializeField] private string sceneName = "Jump minigame";
+    [SerializeField] private AudioSource collectibleSFX;
+    [SerializeField] private TMP_Text treatsTotalText;
+    [SerializeField] private TMP_Text verbText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Treat"))
+        {
+            Destroy(collision.gameObject);
+            treat++;
+            treatsTotalText.text = "Treats: " + treat;
+            collectibleSFX.Play();
+        }
+
+        if (collision.gameObject.CompareTag("Verb"))  
+        {
+            Destroy(collision.gameObject);
+            verb++;
+            //ChangeScene();
+            verbText.text = "Verbs learned: " + verb;
+            collectibleSFX.Play();
+        }
+    }
+
+   // public void ChangeScene(string sceneName)
+   // {
+    //    SceneManager.LoadScene(sceneName);
+   // }
+}
